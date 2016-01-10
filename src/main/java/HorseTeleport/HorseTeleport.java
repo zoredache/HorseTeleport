f49d0bec75f0f1bfae63b5e6435d2342be3310ef
versox <cass.carson@gmail.com>
2016-01-10 18:05:00 -0500

    Created plugin.yml and main class/package

---
+++ src/main/java/HorseTeleport/HorseTeleport.java
@@ -0,0 +1,17 @@
+package HorseTeleport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HorseTeleport extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
